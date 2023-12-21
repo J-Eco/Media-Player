13b9ceb4d05c1bba6737b065378f99499d227895
Jonathan <justezpa@gmail.com>
2023-12-20 22:25:57 -0800

    updated media.js code to be more condensed, and code less repetitive. remove unneed html tags. Updated style sheet

--- javascript/media.js
+++ javascript/media.js
@@ -19,32 +19,42 @@ function handleFiles(files) {
         const video = document.createElement("video");
         video.classList.add("obj");
         video.file = file;
-        video.setAttribute("slot", "media"); // sets needed slot='media' needed for media control
+        video.setAttribute("slot", "media"); // sets needed slot='media' needed for media chrome
         previewDiv.appendChild(video);
 
 
 
         // code here reacts once a mp4/video file is added. This section adds media player controls.
 
-        const mediaCon = document.createElement("media-control-bar");
-        mediaCon.file = file;
-        previewDiv.appendChild(mediaCon);
+        const mediaPlayer = 
+            `<media-control-bar>
 
-        const mediaPlay = document.createElement("media-play-button");
-        mediaPlay.file = file;
-        previewDiv.appendChild(mediaPlay);
+                <media-play-button></media-play-button>
+                
+                <media-seek-forward-button></media-seek-forward-button>
+                
+                <media-seek-backward-button></media-seek-backward-button>
+                
+                <media-mute-button></media-mute-button>
+                
+                <media-volume-range></media-volume-range>
+                
+                <media-time-range></media-time-range>
+                
+                <media-time-display></media-time-display>
+                
+                <media-captions-button></media-captions-button>
+                
+                <media-playback-rate-button></media-playback-rate-button>
+                
+                <media-fullscreen-button></media-fullscreen-button>
+            
+            </media-control-bar>`;
+                            
 
-        const mediaMute = document.createElement("media-mute-button");
-        mediaMute.file = file;
-        previewDiv.appendChild(mediaMute);
-
-        const mediaVolControl = document.createElement("media-volume-range");
-        mediaVolControl.file = file;
-        previewDiv.appendChild(mediaVolControl);
+        document.getElementById("videoPlayer").insertAdjacentHTML("afterbegin", mediaPlayer);
         
-
-        // end
-
+        
         const reader = new FileReader();
         reader.onload = (e) => {
             video.src = e.target.result;
@@ -60,3 +70,38 @@ document.getElementById("myFile").addEventListener("change", function (e) {
         handleFiles(files);
     }
 });
+
+
+// not deleting the bottom. Its a reminder to myself on how not to do it. Originally i did  a repetitive appenchild. The media controls were vertical
+// and messed up. the way above is a lot simpler.
+
+
+        // const mediaCon = document.createElement("media-control-bar");
+        // mediaCon.file = file;
+        // previewDiv.appendChild(mediaCon);
+
+        // const mediaPlay = document.createElement("media-play-button");
+        // mediaPlay.file = file;
+        // previewDiv.appendChild(mediaPlay);
+
+        // const mediaMute = document.createElement("media-mute-button");
+        // mediaMute.file = file;
+        // previewDiv.appendChild(mediaMute);
+
+        // const mediaVolControl = document.createElement("media-volume-range");
+        // mediaVolControl.file = file;
+        // previewDiv.appendChild(mediaVolControl);
+        
+        // const mediaRange = document.createElement("media-time-range");
+        // mediaRange.file = file;
+        // previewDiv.appendChild(mediaRange);
+
+        // const mediaPip = document.createElement("media-pip-button");
+        // mediaPip.file = file;
+        // previewDiv.appendChild(mediaPip);
+
+        // const mediaFull = document.createElement("media-fullscreen-button");
+        // mediaFull.file = file;
+        // previewDiv.appendChild(mediaFull);
+
+        // end
